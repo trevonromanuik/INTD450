e83704ede3bc55cdf1ce61c0e6fab9db198442b1
jechynge <ching.jordan@gmail.com>
2014-03-15 22:55:19 -0600

    Club Interior

---
+++ Hacker/Hacker/Hacker/Levels/ClubInteriorLevel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+
+using Hacker.Components;
+using Hacker.GameObjects;
+using Hacker.Layers;
+using Hacker.Managers;
+
+namespace Hacker.Levels
+{
+    class ClubInteriorLevel : Level
+    {
+        public ClubInteriorLevel()
+            : base()
+        {
+            PushLayer(new MapLayer("club_interior"));
+            PushLayer(new CollisionLayer("club_interior_collision"));
+
+            ObjectLayer objectLayer = new ObjectLayer();
+            objectLayer.GameObjectManager.AddGameObject(Player.Instance);
+            Player.Instance.GetComponent<Position>().Teleport(128, 320);
+
+            PushLayer(objectLayer);
+        }
+    }
+}
